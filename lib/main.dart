7408f7901e95637fcb871b0189ea63b1f197411a
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-12-18 04:43:16 +0300

    Added the children

--- lib/main.dart
+++ lib/main.dart
@@ -10,47 +10,50 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Custom Button Example"),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: ElevatedButton(
-          style: ButtonStyle(
-            backgroundColor: WidgetStateProperty.resolveWith<Color>(
-              (Set<WidgetState> states) {
-                if (states.contains(WidgetState.pressed)) {
-                  return const Color.fromRGBO(32, 181, 115, 100)
-                      .withValues(alpha: 0.8);
-                }
-                return const Color.fromRGBO(32, 181, 115, 100);
+        appBar: AppBar(
+          title: Text("Custom Button Example"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Column(children: [
+            Image(image: AssetImage(assetName)),
+            ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: WidgetStateProperty.resolveWith<Color>(
+                  (Set<WidgetState> states) {
+                    if (states.contains(WidgetState.pressed)) {
+                      return const Color.fromRGBO(32, 181, 115, 100)
+                          .withValues(alpha: 0.8);
+                    }
+                    return const Color.fromRGBO(32, 181, 115, 100);
+                  },
+                ),
+                foregroundColor: WidgetStateProperty.all<Color>(Colors.white),
+                overlayColor: WidgetStateProperty.all<Color>(
+                  Colors.white.withValues(alpha: 0.2),
+                ),
+                shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                ),
+              ),
+              onPressed: () {
+                print("Button Pressed!");
               },
-            ),
-            foregroundColor: WidgetStateProperty.all<Color>(Colors.white),
-            overlayColor: WidgetStateProperty.all<Color>(
-              Colors.white.withValues(alpha: 0.2),
-            ),
-            shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-              RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30.0),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 12, horizontal: 24),
+                child: Text(
+                  'Find a Machine',
+                  style: TextStyle(
+                      fontFamily: "Inter",
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
               ),
             ),
-          ),
-          onPressed: () {
-            print("Button Pressed!");
-          },
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 24),
-            child: Text(
-              'Find a Machine',
-              style: TextStyle(
-                  fontFamily: "Inter",
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold),
-            ),
-          ),
-        ),
-      ),
-    );
+          ]),
+        ));
   }
 }
