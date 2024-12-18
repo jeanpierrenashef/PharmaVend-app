51afb8093272d9fa143fd31b2a54654ad0cb4076
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-12-18 03:40:20 +0300

    Replaced the opacity with values

--- lib/main.dart
+++ lib/main.dart
@@ -13,20 +13,20 @@ void main() {
             backgroundColor: WidgetStateProperty.resolveWith<Color>(
               (Set<WidgetState> states) {
                 if (states.contains(WidgetState.pressed)) {
-                  return Colors.blue.withValues(alpha: 0.8); // When pressed
+                  return Colors.blue.withValues(alpha: 0.8);
                 }
                 if (states.contains(WidgetState.hovered)) {
-                  return Colors.blue.withValues(alpha: 0.6); // When hovered
+                  return Colors.blue.withValues(alpha: 0.6);
                 }
                 if (states.contains(WidgetState.focused)) {
-                  return Colors.blue.withValues(alpha: 0.7); // When focused
+                  return Colors.blue.withValues(alpha: 0.7);
                 }
-                return Colors.blue; // Default color
+                return Colors.blue;
               },
             ),
             foregroundColor: WidgetStateProperty.all<Color>(Colors.white),
             overlayColor: WidgetStateProperty.all<Color>(
-              Colors.white.withValues(alpha: 0.2), // Ripple effect color
+              Colors.white.withValues(alpha: 0.2),
             ),
             shape: WidgetStateProperty.all<RoundedRectangleBorder>(
               RoundedRectangleBorder(
