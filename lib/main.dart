6794663c481270790a28ca8b78ce4a17504c107d
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-12-19 02:51:19 +0300

    Added the maps display widget

--- lib/main.dart
+++ lib/main.dart
@@ -101,3 +101,43 @@ class Home extends StatelessWidget {
         ));
   }
 }
+
+class Maps extends StatefulWidget {
+  const Maps({super.key});
+
+  @override
+  State<Maps> createState() => _MapsState();
+}
+
+class _MapsState extends State<Maps> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.green[700],
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Maps Sample App'),
+          elevation: 2,
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
