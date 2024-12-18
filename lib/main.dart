6b678c19695aa458a0ea5b4703ff74ec3ba8286b
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-12-18 05:07:50 +0300

    Modified the background color

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.white,
         appBar: AppBar(
           title: Text("Custom Button Example"),
           centerTitle: true,
