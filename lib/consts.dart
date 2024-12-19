fd2cd6ed69c80990daa6ea48ebbdc88fb915977a
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-12-19 11:28:42 +0300

    Major changes: location cannot be automatically grabbed and i worked according to that

--- lib/consts.dart
+++
@@ -1,1 +0,0 @@
-const String GOOGLE_MAPS_API_KEY = "AIzaSyD3EiOtcu7hbFzjSOBIMlFbnU7pXSCq4cw";
--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,11 +1,10 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_application/consts.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -47,12 +46,16 @@ class _MapPageState extends State<MapPage> {
                 target: _Hamra,
                 zoom: 9,
               ),
+              myLocationEnabled:
+                  true, // Enables the blue circle for user location
+              myLocationButtonEnabled:
+                  true, // Adds the location button on the map
               markers: {
-                Marker(
-                  markerId: MarkerId("_sourceLocation"),
-                  icon: BitmapDescriptor.defaultMarker,
-                  position: _currentP!,
-                ),
+                // Marker(
+                //   markerId: MarkerId("_sourceLocation"),
+                //   icon: BitmapDescriptor.defaultMarker,
+                //   position: _currentP!,
+                // ),
                 Marker(
                   markerId: MarkerId("_JbeilLocation"),
                   icon: BitmapDescriptor.defaultMarker,
@@ -80,7 +83,7 @@ class _MapPageState extends State<MapPage> {
     final GoogleMapController controller = await _mapController.future;
     CameraPosition newCameraPosition = CameraPosition(
       target: pos,
-      zoom: 13,
+      zoom: 9,
     );
     await controller.animateCamera(
       CameraUpdate.newCameraPosition(newCameraPosition),
@@ -119,9 +122,9 @@ class _MapPageState extends State<MapPage> {
 
   Future<void> _updatePolyline() async {
     if (_currentP == null) return;
-
+    final googleMapsApiKey = dotenv.env['GOOGLE_MAPS_API_KEY']!;
     PolylineResult result = await _polylinePoints.getRouteBetweenCoordinates(
-      googleApiKey: GOOGLE_MAPS_API_KEY,
+      googleApiKey: googleMapsApiKey,
       request: PolylineRequest(
         origin: PointLatLng(_currentP!.latitude, _currentP!.longitude),
         destination: PointLatLng(_Hamra.latitude, _Hamra.longitude),
@@ -138,46 +141,5 @@ class _MapPageState extends State<MapPage> {
     } else {
       print("Error retrieving polyline: ${result.errorMessage}");
     }
-  }
-
-  /// Determine the current position of the device.
-  ///
-  /// When the location services are not enabled or permissions
-  /// are denied the `Future` will return an error.
-  Future<Position> _determinePosition() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    // Test if location services are enabled.
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      // Location services are not enabled don't continue
-      // accessing the position and request users of the
-      // App to enable the location services.
-      return Future.error('Location services are disabled.');
-    }
-
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        // Permissions are denied, next time you could try
-        // requesting permissions again (this is also where
-        // Android's shouldShowRequestPermissionRationale
-        // returned true. According to Android guidelines
-        // your App should show an explanatory UI now.
-        return Future.error('Location permissions are denied');
-      }
-    }
-
-    if (permission == LocationPermission.deniedForever) {
-      // Permissions are denied forever, handle appropriately.
-      return Future.error(
-          'Location permissions are permanently denied, we cannot request permissions.');
-    }
-
-    // When we reach here, permissions are granted and we can
-    // continue accessing the position of the device.
-    return await Geolocator.getCurrentPosition();
   }
 }
