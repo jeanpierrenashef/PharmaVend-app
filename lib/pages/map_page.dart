8b205fd486bc59bb72c931b6b3eec98c7ec297b1
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-12-19 04:10:18 +0300

    Added the markers for several machines

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -10,14 +10,31 @@ class MapPage extends StatefulWidget {
 
 class _MapPageState extends State<MapPage> {
   static const LatLng _pGooglePlex = LatLng(33.833905, 35.591587);
+  static const LatLng _Jbeil = LatLng(34.115568, 35.674343);
+  static const LatLng _Hamra = LatLng(33.896198, 35.477865);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: GoogleMap(
-            initialCameraPosition: CameraPosition(
-      target: _pGooglePlex,
-      zoom: 9,
-    )));
+      initialCameraPosition: CameraPosition(
+        target: _pGooglePlex,
+        zoom: 9,
+      ),
+      markers: {
+        Marker(
+            markerId: MarkerId("_currentLocation"),
+            icon: BitmapDescriptor.defaultMarker,
+            position: _pGooglePlex),
+        Marker(
+            markerId: MarkerId("_JbeilLocation"),
+            icon: BitmapDescriptor.defaultMarker,
+            position: _Jbeil),
+        Marker(
+            markerId: MarkerId("_HamraLocation"),
+            icon: BitmapDescriptor.defaultMarker,
+            position: _Hamra),
+      },
+    ));
   }
 }
