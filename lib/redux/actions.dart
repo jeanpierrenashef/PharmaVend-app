6e857d20ba1d705bd02d1b0fa2ee44420f33757a
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-12-22 05:49:08 +0300

    Created the action for fetching the products

---
+++ lib/redux/actions.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_application/models/product.dart';
+
+class loadProductsAction {}
+
+class loadProductsSuccessAction {
+  final List<Product> products;
+  loadProductsSuccessAction(this.products);
+}
+
+class loadProductsFailureAction {
+  final String error;
+  loadProductsFailureAction(this.error);
+}
