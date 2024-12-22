378328116a8738c349a0f44aab6255e29d9bce92
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-12-22 22:17:08 +0300

    Adjusted all the things that need fixing

--- lib/redux/app_state.dart
+++ lib/redux/app_state.dart
@@ -8,7 +8,7 @@ class AppState {
   AppState({this.products = const [], this.cart = const [], this.error});
 
   AppState copyWith(
-      {List<Product>? products, List<Product>? cart, String? error}) {
+      {List<Product>? products, List<CartItem>? cart, String? error}) {
     return AppState(
         products: this.products, cart: this.cart, error: this.error);
   }
