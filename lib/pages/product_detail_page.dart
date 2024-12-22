7dc17c6b2fd18cc6ab96689735c92e3c60b77f6d
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-12-22 09:52:08 +0300

    Modifying the product detail page

--- lib/pages/product_detail_page.dart
+++ lib/pages/product_detail_page.dart
@@ -13,101 +13,108 @@ class ProductDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: CustomAppBar(),
-          backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: const Text(
-                  "Hamra V12",
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: CustomAppBar(),
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: const Text(
+                "Hamra V12",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
             ),
-            Expanded(
-              child: StoreConnector<AppState, Product?>(
-                converter: (store) {
-                  return store.state.products.firstWhere(
-                    (product) => product.id == productId,
+          ),
+          Expanded(
+            child: StoreConnector<AppState, Product?>(
+              converter: (store) {
+                return store.state.products.firstWhere(
+                  (product) => product.id == productId,
+                );
+              },
+              builder: (context, product) {
+                if (product == null) {
+                  return const Center(
+                    child: Text('Product not found.'),
                   );
-                },
-                builder: (context, product) {
-                  if (product == null) {
-                    return const Center(
-                      child: Text('Product not found.'),
-                    );
-                  }
+                }
 
-                  return Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Image.network(
-                          product.image,
-                          height: 200,
-                          fit: BoxFit.cover,
-                        ),
-                        const SizedBox(height: 16),
-                        Text(
-                          product.name,
-                          style: const TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
+                return Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Image.network(
+                        product.image,
+                        height: 200,
+                        fit: BoxFit.cover,
+                      ),
+                      const SizedBox(height: 16),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            product.name,
+                            style: const TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
-                        ),
-                        const SizedBox(height: 8),
-                        Text(
-                          product.description,
-                          style: const TextStyle(fontSize: 16),
-                        ),
-                        const SizedBox(height: 16),
-                        Text(
-                          "Category: ${product.category}",
-                          style:
-                              const TextStyle(fontSize: 16, color: Colors.grey),
-                        ),
-                        const SizedBox(height: 8),
-                        Text(
-                          "Price: \$${product.price.toStringAsFixed(2)}",
-                          style: const TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
+                          Text(
+                            "\$${product.price.toStringAsFixed(2)}",
+                            style: const TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.w600,
+                            ),
                           ),
-                        ),
-                        const SizedBox(height: 16),
-                        ElevatedButton(
+                        ],
+                      ),
+                      const SizedBox(height: 8),
+                      Text(
+                        product.description,
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                      const Spacer(),
+                      Center(
+                        child: ElevatedButton(
                           onPressed: () {
                             // Handle Add to Cart
                           },
                           style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.green,
+                            backgroundColor:
+                                const Color.fromRGBO(32, 181, 115, 1),
                             shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(12.0),
+                              borderRadius: BorderRadius.circular(30.0),
                             ),
                             padding: const EdgeInsets.symmetric(
                                 vertical: 12, horizontal: 24),
                           ),
                           child: const Text(
                             'Add to Cart',
-                            style: TextStyle(fontSize: 18),
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.white,
+                            ),
                           ),
                         ),
-                      ],
-                    ),
-                  );
-                },
-              ),
-            )
-          ],
-        ));
+                      ),
+                      const SizedBox(height: 16),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
