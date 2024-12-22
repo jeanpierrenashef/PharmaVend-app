2fe053b8a425790d4d4925400ec192167bb1b322
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-12-22 05:54:53 +0300

    Adjusted the app_state file

--- lib/redux/app_state.dart
+++ lib/redux/app_state.dart
@@ -2,9 +2,10 @@ import 'package:flutter_application/models/product.dart';
 
 class AppState {
   final List<Product> products;
+  final String? error;
 
-  AppState({required this.products});
-  AppState copyWith({List<Product>? products}) {
-    return AppState(products: this.products);
+  AppState({required this.products, this.error});
+  AppState copyWith({List<Product>? products, String? error}) {
+    return AppState(products: this.products, error: this.error);
   }
 }
