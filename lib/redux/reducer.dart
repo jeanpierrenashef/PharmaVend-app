b9c0ac8e03d53ae9fbaa4f74ba6376b1102cde37
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-12-22 06:10:42 +0300

    Modifications to the reducer

---
+++ lib/redux/reducer.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_application/redux/actions.dart';
+import 'package:flutter_application/redux/app_state.dart';
+
+AppState loadProductsReducer(AppState state, dynamic action) {
+  if (action is loadProductsAction) {
+    return state.copyWith(error: null);
+  } else if (action is loadProductsSuccessAction) {
+    return state.copyWith(products: action.products, error: null);
+  } else if (action is loadProductsFailureAction) {
+    return state.copyWith(error: action.error);
+  }
+  return state;
+}
