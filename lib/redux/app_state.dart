f86faf8f2a5b28e2619e5acf4f0e329906d01bc1
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-12-22 16:37:18 +0300

    Added the cart to the state

--- lib/redux/app_state.dart
+++ lib/redux/app_state.dart
@@ -2,10 +2,14 @@ import 'package:flutter_application/models/product.dart';
 
 class AppState {
   final List<Product> products;
+  final List<Product> cart;
   final String? error;
 
-  AppState({required this.products, this.error});
-  AppState copyWith({List<Product>? products, String? error}) {
-    return AppState(products: this.products, error: this.error);
+  AppState({this.products = const [], this.cart = const [], this.error});
+
+  AppState copyWith(
+      {List<Product>? products, List<Product>? cart, String? error}) {
+    return AppState(
+        products: this.products, cart: this.cart, error: this.error);
   }
 }
