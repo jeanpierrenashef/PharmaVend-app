a6b28c4efb72f9b9d034f39d65a9c1d7657c5559
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-12-22 05:45:43 +0300

    Created the product model and populated it

---
+++ lib/models/product.dart
@@ -0,0 +1,17 @@
+class Product {
+  final String id;
+  final String name;
+  final String description;
+  final String category;
+  final double price;
+  final String image;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.category,
+    required this.price,
+    required this.image,
+  });
+}
