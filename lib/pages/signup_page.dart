98881a178e86375a12a6797bebf98f0219df5cf0
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-12-24 14:06:00 +0300

    Added title and create account text

--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -9,17 +9,35 @@ class SignupPage extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SingleChildScrollView(
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 40),
-            height: MediaQuery.of(context).size.height - 50,
-            width: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[],
-            ),
-          ),
-        ),
+            child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 40),
+                height: MediaQuery.of(context).size.height - 50,
+                width: double.infinity,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Column(
+                        children: <Widget>[
+                          const SizedBox(height: 60.0),
+                          const Text(
+                            "Sign up",
+                            style: TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            "Create your account",
+                            style: TextStyle(
+                                fontSize: 15, color: Colors.grey[700]),
+                          )
+                        ],
+                      ),
+                    ]))),
       ),
     );
   }
