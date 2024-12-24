0e0ea96d3c793c0c34fec6323ddb5c2a335b702d
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-12-24 14:09:02 +0300

    Added the email input

--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -9,52 +9,60 @@ class SignupPage extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SingleChildScrollView(
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 40),
-            height: MediaQuery.of(context).size.height - 50,
-            width: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Column(
-                  children: <Widget>[
-                    const SizedBox(height: 60.0),
-                    const Text(
-                      "Sign up",
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
+            child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 40),
+                height: MediaQuery.of(context).size.height - 50,
+                width: double.infinity,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Column(
+                        children: <Widget>[
+                          const SizedBox(height: 60.0),
+                          const Text(
+                            "Sign up",
+                            style: TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            "Create your account",
+                            style: TextStyle(
+                                fontSize: 15, color: Colors.grey[700]),
+                          )
+                        ],
                       ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Text(
-                      "Create your account",
-                      style: TextStyle(fontSize: 15, color: Colors.grey[700]),
-                    )
-                  ],
-                ),
-                Column(
-                  children: <Widget>[
-                    TextField(
-                      decoration: InputDecoration(
-                          hintText: "Username",
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(18),
-                              borderSide: BorderSide.none),
-                          fillColor: Color.fromRGBO(32, 181, 115, 0.1),
-                          filled: true,
-                          prefixIcon: const Icon(Icons.person)),
-                    ),
-                    const SizedBox(height: 20),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ),
+                      Column(
+                        children: <Widget>[
+                          TextField(
+                            decoration: InputDecoration(
+                                hintText: "Username",
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(18),
+                                    borderSide: BorderSide.none),
+                                fillColor: Colors.purple.withOpacity(0.1),
+                                filled: true,
+                                prefixIcon: const Icon(Icons.person)),
+                          ),
+                          const SizedBox(height: 20),
+                          TextField(
+                            decoration: InputDecoration(
+                                hintText: "Email",
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(18),
+                                    borderSide: BorderSide.none),
+                                fillColor: Colors.purple.withOpacity(0.1),
+                                filled: true,
+                                prefixIcon: const Icon(Icons.email)),
+                          ),
+                        ],
+                      ),
+                    ]))),
       ),
     );
   }
