9006d3030d7ae56bedb463b84436b2751e1dd9e6
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-12-24 13:43:24 +0300

    Added the input filed of login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -15,7 +15,6 @@ class LoginPage extends StatelessWidget {
             children: [
               _header(context),
               _inputField(context),
-              _signup(context),
             ],
           ),
         ),
@@ -75,21 +74,6 @@ class LoginPage extends StatelessWidget {
             style: TextStyle(fontSize: 20),
           ),
         )
-      ],
-    );
-  }
-
-  _signup(context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const Text("Dont have an account? "),
-        TextButton(
-            onPressed: () {},
-            child: const Text(
-              "Sign Up",
-              style: TextStyle(color: Color.fromRGBO(32, 181, 115, 1)),
-            ))
       ],
     );
   }
