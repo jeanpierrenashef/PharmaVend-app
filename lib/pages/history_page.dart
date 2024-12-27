c08d2377de3e225999fe7c43754bcd749aeeda85
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-12-27 05:36:37 +0300

    Initializing the sotre connector to get the transactions list and the machines for later corolation

--- lib/pages/history_page.dart
+++ lib/pages/history_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/custom/app_bar.dart';
 import 'package:flutter_application/custom/nav_bar.dart';
 import 'package:flutter_application/models/machine.dart';
 import 'package:flutter_application/models/transaction.dart';
@@ -36,43 +37,62 @@ class _HistoryPageState extends State<HistoryPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: const CustomAppBar(),
+          backgroundColor: Colors.white,
+          automaticallyImplyLeading: false,
+        ),
+        body: StoreConnector<AppState, List<Transaction>>(
+            converter: (store) => store.state.transactions,
+            builder: (context, transactions) {
+              if (transactions.isEmpty) {
+                return const Center(
+                  child: Text("No History"),
+                );
+              }
+              final machines =
+                  StoreProvider.of<AppState>(context).state.machines;
+              final groupedTransactions =
+                  _groupByDateAndLocation(transactions, machines);
+            }),
         bottomNavigationBar: CustomBottomNavBar(
-      selectedIndex: 2,
-      onItemTapped: (index) {
-        switch (index) {
-          case 0:
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(builder: (context) => ProductPage()),
-            );
-            break;
-          case 1:
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(builder: (context) => const MapPage()),
-            );
-            break;
-          case 2:
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(builder: (context) => HistoryPage()),
-            );
-            break;
-          case 3:
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(builder: (context) => CartPage()),
-            );
-            break;
-          case 4:
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(builder: (context) => CartPage()),
-            );
-            break;
-        }
-      },
-    ));
+          selectedIndex: 2,
+          onItemTapped: (index) {
+            switch (index) {
+              case 0:
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => ProductPage()),
+                );
+                break;
+              case 1:
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const MapPage()),
+                );
+                break;
+              case 2:
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => HistoryPage()),
+                );
+                break;
+              case 3:
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => CartPage()),
+                );
+                break;
+              case 4:
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => CartPage()),
+                );
+                break;
+            }
+          },
+        ));
   }
 
   Map<String, List<Transaction>> _groupByDateAndLocation(
