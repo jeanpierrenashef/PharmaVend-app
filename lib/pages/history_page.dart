e573f8d99efdc1ea5b6f845fbd0b23290f7f04c1
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-12-27 05:20:00 +0300

    Added the fetch transactions function

--- lib/pages/history_page.dart
+++ lib/pages/history_page.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/redux/app_state.dart';
+import 'package:flutter_application/services/transaction_service.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 
-class HistoryPage extends StatelessWidget {
-  const HistoryPage({super.key});
+class HistoryPage extends StatefulWidget {
+  const HistoryPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  State<HistoryPage> createState() => _HistoryPageState();
+
+  class _HistoryPageState extends State<HistoryPage>{
+    @override
+    void didChangeDependencies() {
+      super.didChangeDependencies();
+      _fetchTransactions();
+    }
+
+  void _fetchTransactions() {
+    final store = StoreProvider.of<AppState>(context);
+    const userId = 1; 
+    TransactionService.fetchTransactions(store, userId).then((_) {
+      final transactions = store.state.transactions;
+      print(
+          "Transactions fetched in HistoryPage: ${transactions.map((t) => t.id).toList()}");
+    });
+  }
   }
 }
+
