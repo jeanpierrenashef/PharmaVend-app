88471cddff4aaa5cdf623cc470d706836f7e6b9e
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-12-27 05:24:23 +0300

    Added the navigator bar

--- lib/pages/history_page.dart
+++ lib/pages/history_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/custom/nav_bar.dart';
+import 'package:flutter_application/pages/cart_page.dart';
+import 'package:flutter_application/pages/map_page.dart';
+import 'package:flutter_application/pages/products_page.dart';
 import 'package:flutter_application/redux/app_state.dart';
 import 'package:flutter_application/services/transaction_service.dart';
 import 'package:flutter_redux/flutter_redux.dart';
@@ -29,6 +33,43 @@ class _HistoryPageState extends State<HistoryPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        bottomNavigationBar: CustomBottomNavBar(
+      selectedIndex: 2,
+      onItemTapped: (index) {
+        switch (index) {
+          case 0:
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => ProductPage()),
+            );
+            break;
+          case 1:
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => const MapPage()),
+            );
+            break;
+          case 2:
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => HistoryPage()),
+            );
+            break;
+          case 3:
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => CartPage()),
+            );
+            break;
+          case 4:
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => CartPage()),
+            );
+            break;
+        }
+      },
+    ));
   }
 }
