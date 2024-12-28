2773c077493b84329267ee3c9d3dca137fddf8ab
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-12-28 11:12:01 +0300

    Added the custom nav bar in the dispense page

--- lib/pages/dispense_page.dart
+++ lib/pages/dispense_page.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application/custom/app_bar.dart';
+import 'package:flutter_application/custom/nav_bar.dart';
+import 'package:flutter_application/pages/cart_page.dart';
+import 'package:flutter_application/pages/history_page.dart';
+import 'package:flutter_application/pages/map_page.dart';
+import 'package:flutter_application/pages/products_page.dart';
 
 class DispensePage extends StatefulWidget {
   const DispensePage({super.key});
@@ -30,6 +35,43 @@ class _DispensePageState extends State<DispensePage> {
             ),
           ],
         ),
+      ),
+      bottomNavigationBar: CustomBottomNavBar(
+        selectedIndex: 4,
+        onItemTapped: (index) {
+          switch (index) {
+            case 0:
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => ProductPage()),
+              );
+              break;
+            case 1:
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const MapPage()),
+              );
+              break;
+            case 2:
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => HistoryPage()),
+              );
+              break;
+            case 3:
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => CartPage()),
+              );
+              break;
+            case 4:
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => DispensePage()),
+              );
+              break;
+          }
+        },
       ),
     );
   }
