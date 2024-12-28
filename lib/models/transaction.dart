6fc8df5e24328f058dc3292c98106b4438c853cb
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-12-28 15:16:59 +0300

    Added the copy with method in the transaction model

--- lib/models/transaction.dart
+++ lib/models/transaction.dart
@@ -8,15 +8,16 @@ class Transaction {
   final String updatedAt;
   final int dispensed;
 
-  Transaction(
-      {required this.id,
-      required this.quantity,
-      required this.totalPrice,
-      required this.userId,
-      required this.machineId,
-      required this.productId,
-      required this.updatedAt,
-      required this.dispensed});
+  Transaction({
+    required this.id,
+    required this.quantity,
+    required this.totalPrice,
+    required this.userId,
+    required this.machineId,
+    required this.productId,
+    required this.updatedAt,
+    required this.dispensed,
+  });
 
   factory Transaction.fromJson(Map<String, dynamic> json) {
     return Transaction(
@@ -28,6 +29,28 @@ class Transaction {
       productId: json['product_id'],
       updatedAt: json['updated_at'],
       dispensed: json['dispensed'],
+    );
+  }
+
+  Transaction copyWith({
+    int? id,
+    int? quantity,
+    double? totalPrice,
+    int? userId,
+    int? machineId,
+    int? productId,
+    String? updatedAt,
+    int? dispensed,
+  }) {
+    return Transaction(
+      id: id ?? this.id,
+      quantity: quantity ?? this.quantity,
+      totalPrice: totalPrice ?? this.totalPrice,
+      userId: userId ?? this.userId,
+      machineId: machineId ?? this.machineId,
+      productId: productId ?? this.productId,
+      updatedAt: updatedAt ?? this.updatedAt,
+      dispensed: dispensed ?? this.dispensed,
     );
   }
 }
