4322c3d5f0fa7d314af57afa1f5ae1206dbfd28e
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-12-28 12:22:56 +0300

    Added the dispense transaction service

---
+++ lib/services/dispense_service.dart
@@ -0,0 +1,19 @@
+import 'package:http/http.dart' as http;
+
+class DispenseService {
+  static Future<void> dispenseTransaction(int transactionId) async {
+    try {
+      final response = await http.put(
+        Uri.parse("http://192.168.1.7:8000/api/dispense"),
+        headers: {"Content-Type": "application/json"},
+      );
+      if (response.statusCode == 200) {
+        print("Dispense successful");
+      } else {
+        print("Failed to update the transactions table");
+      }
+    } catch (e) {
+      print("Error updating: $e");
+    }
+  }
+}
