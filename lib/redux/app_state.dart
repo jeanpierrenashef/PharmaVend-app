f1201e8ad5816e9456aadea53cbd63027976546f
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2025-01-06 02:06:29 +0300

    Modified the app state to add the user

--- lib/redux/app_state.dart
+++ lib/redux/app_state.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_application/models/machine.dart';
 import 'package:flutter_application/models/product.dart';
 import 'package:flutter_application/models/transaction.dart';
+import 'package:flutter_application/models/user.dart';
 
 class AppState {
   final List<Product> products;
@@ -8,26 +9,30 @@ class AppState {
   final List<Machine> machines;
   final List<Transaction> transactions;
   final String? error;
+  final User? user;
 
   AppState(
       {this.products = const [],
       this.cart = const [],
       this.machines = const [],
       this.transactions = const [],
-      this.error});
+      this.error,
+      this.user});
 
   AppState copyWith(
       {List<Product>? products,
       List<CartItem>? cart,
       List<Machine>? machines,
       List<Transaction>? transactions,
-      String? error}) {
+      String? error,
+      User? user}) {
     return AppState(
       products: products ?? this.products,
       cart: cart ?? this.cart,
       machines: machines ?? this.machines,
       transactions: transactions ?? this.transactions,
       error: error ?? this.error,
+      user: user ?? this.user,
     );
   }
 
