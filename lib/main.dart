b08bb98685a537039de0360076403aae33522c39
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2025-01-14 17:40:53 +0300

    Minor modifications

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,6 @@ import 'package:location/location.dart';
 import 'package:redux/redux.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
